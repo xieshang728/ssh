a9b68966f49a3671d4b931051b3eed533518311c
xieshang728 <xieshang728@163.com>
2017-11-07 21:44:44 +0800

    xies

---
+++ src/demo/MyThread.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by Administrator on 2017/11/7.
+ */
+public class MyThread {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
