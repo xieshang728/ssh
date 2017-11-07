9127ac593121eec2a851c47d3e4fe1d3dfaabe69
xieshang728 <xieshang728@163.com>
2017-11-07 21:48:47 +0800

    xies

---
+++ src/demo/MyThread2.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * Created by Administrator on 2017/11/7.
+ * @author xies
+ */
+
+public class MyThread2 {
+    public static void main(String[] args){
+        System.out.println("hell oworld");
+    }
+}
